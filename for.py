507f566ad9ec55086e4eab80f5c0b04dc23f5466
randyvtran@yahoo.com <randyvtran@yahoo.com>
2020-02-26 20:43:12 -0800

    made few changes to for.py and readme

--- for.py
+++ for.py
@@ -3,7 +3,7 @@
 # This is an exercise file from Python 3 Essential Training on lynda.com
 # Copyright 2010 The BearHeart Group, LLC
 
-""" Add new func main() """ 	"""dev A"""
+""" Add new func main() ::  dev A"""
 def main():
     fh = open('lines.txt')
     for line in fh.readlines():
--- temp/ReadMe.txt
+++ temp/ReadMe.txt
@@ -1,3 +1,5 @@
 Hello world
 
 This is a test
+
+++++++++++++++++
